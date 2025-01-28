6c33c6302ef5fda150653ef62afc4d51b04d3dbb
unknown <paul-emmanuel.buffe@laplateforme.io>
2025-01-28 20:13:36 +0100

    sounds, background image, and scoring complete

--- ninja_fruits.py
+++ ninja_fruits.py
@@ -1,23 +1,42 @@
+import random as rd
 import pygame
+import os
+import sys
+
+# ways across the project
+BASE_DIR = r"C:/Users/Windows/Desktop/projets/1a/ninja_fruits"
+IMAGE_DIR = os.path.join(BASE_DIR, "images")
+SOUND_DIR = os.path.join(BASE_DIR, "sounds")
 
 
-# Initialisation de Pygame
 pygame.init()
 
-# Configuration de l'écran
+# screen setting
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Projet Pygame - 1200x600")
+pygame.display.set_caption("Ninjas Fruits")
 
-# Couleurs
+# Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BACKGROUND_COLOR = (50, 50, 150)  
 
+# Pictures
+back_ground_image  = pygame.image.load(os.path.join(IMAGE_DIR,"background_image.png"))
 
+#fonts used
+ubuntu_font=pygame.font.Font(os.path.join(BASE_DIR, "Ubuntu-Regular.ttf"), 36)
 
-# Boucle principale
+# sounds
+try:
+    back_ground_sound = pygame.mixer.Sound(r"C:\Users\Windows\Desktop\projets\1a\ninja_fruits\sounds\ninja.wav" )
+except pygame.error as e:
+    print(f"Error loading sound: {e}")
+    back_ground_sound = None
+
+# Main
+back_ground_sound.play(-1)
 running = True
 while running:
     # Gestion des événements
@@ -26,7 +45,7 @@ while running:
             running = False
 
     # Affichage
-    screen.fill(BACKGROUND_COLOR)  # Remplir l'écran avec la couleur de fond
+    screen.blit(back_ground_image, (0,0))
 
     # Mettre à jour l'écran
     pygame.display.flip()
@@ -34,5 +53,4 @@ while running:
 
 # Quitter Pygame proprement
 pygame.quit()
-
 
