48810136ce8665b16a7f00ae3f1c61d8fbaf2714
alexandre <alexandre.chevalier@laplateforme.io>
2025-01-29 11:35:30 +0100

    i rewrote my Fruit class

--- function.py
+++ function.py
@@ -15,101 +15,41 @@ class Fruit:
         self.etat = etat
         self.valeur = valeur #score pour les fruits et valeur strike pour la bombe
         self.letter = letter
-        
-letters =  ["z","q","s","d","o","k","l","m"]
-def letter_tab(letters):
-        return letters[random.randint(0, len(letters)-1)]
-            
-class Apple(Fruit):
-    ## Fruit.name est == a self.init
-    def __init__(self):
-        self.name = "apple"
-        self.x = 0
-        self.y= 600
-        self.width = 75
-        self.length = 75
-        self.letter = letter_tab(letters)
-        self.etat = "pas couper"
-        self.goal1 = random.randint(601, 1025)
-        
 
-    def rect(self, screen, Black):
-       return pygame.draw.rect(screen, Black, (self.x, self.y , self.width, self.length))
-
+    def rect(self, screen, Black, font):
+        fruit = pygame.draw.rect(screen, Black, (self.x, self.y , self.width, self.length))
+        font_dis2 = font.render(self.letter, 1, WHITE)
+        font_rect2 = font_dis2.get_rect(center = fruit.center)
+        screen.blit(font_dis2, font_rect2)
     
     def apple_move_left_right(self):
-        while self.x < 600 and self.y > 100:
-            self.x +=0.10
-            self.y -=0.10
+        print("hello")
+        self.x +=0.10
+        self.y -=0.10
         return self.x, self.y
+    
+
+    letters =  ["z","q","s","d","o","k","l","m"]
+    def letter_tab(letters):
+        return letters[random.randint(0, len(letters)-1)]
             
-
-         
-
-class Pineapple(Fruit):
-        Fruit.name = "pineapple"
-        Fruit.x = 0
-        Fruit.y = 600
-        Fruit.Width = 75
-        Fruit.length = 75
-        Fruit.letter = letter_tab(letters)
-        Fruit.etat = "pas couper"
-        print(Fruit.letter)
-
-        def rect(x, y , width, length):
-            return (x,y,width,length)
-
-class Coconut(Fruit):
-        Fruit.name = "coconut"
-        Fruit.x = 0
-        Fruit.y = 600
-        Fruit.Width = 75
-        Fruit.length = 75
-        Fruit.letter = letter_tab(letters)
-        Fruit.etat = "pas couper"
-        print(Fruit.letter)
-
-        def rect(x, y , width, length):
-           return (x,y,width,length)
-
-class Bomb(Fruit):
-        Fruit.name = "bomb"
-        Fruit.x = 0
-        Fruit.y = 600
-        Fruit.Width = 75
-        Fruit.length = 75
-        Fruit.letter = letter_tab(letters)
-        Fruit.etat = "pas couper"
-        Fruit.valeur = 3 ## 3 strike
-
-
-        def rect(x, y , width, length):
-           return (x,y,width,length)
-
-class Icecube(Fruit):
-        Fruit.name = "icecube"
-        Fruit.x = 0
-        Fruit.y = 600
-        Fruit.Width = 75
-        Fruit.length = 75
-        Fruit.letter = letter_tab(letters)
-        Fruit.etat = "pas couper"
-        print(Fruit.etat)
-
-        def rect(x, y , width, length):
-           return (x,y,width,length)
-
 
 # Configuration de l'écran
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Projet Pygame - 1200x600")
-apple = Apple()
+my_Font = pygame.font.SysFont("comicsansms", 32)
+
+
 # Couleurs
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BACKGROUND_COLOR = (50, 50, 150)  
+
+apple       = Fruit("apple",SCREEN_WIDTH, SCREEN_HEIGHT,75,75, 'pasteque.png', 1,  )
+pineapple   = Fruit()
+coconut     = Fruit()
 
 # Boucle principale
 running = True
@@ -122,9 +62,7 @@ while running:
     # Affichage
     screen.fill(BACKGROUND_COLOR)  # Remplir l'écran avec la couleur de fond
 
-    apple.rect(screen, BLACK)
-    apple.apple_move_left_right()
-    screen.blit(screen, (SCREEN_WIDTH,SCREEN_HEIGHT))
+    
 
 
 
